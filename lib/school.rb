432ceac129322cf46e8ffff9824cee72c5494ecb
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-05-22 00:27:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+  attr_reader :name, :roster, :student 
+  
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end
+  
+  def add_student=(student_name, grade)
+  @student_name = student_name
+  @grade = grade
+  @roster[@grade] ||= []
+  @roster[@grade] << student_name
+ end 
+  
+end  
