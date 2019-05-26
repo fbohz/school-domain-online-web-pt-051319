0ab822dc1d9a7a4013d07453eae77f280386d5f9
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-05-26 18:56:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,12 +9,12 @@ class School
   def add_student(student_name, grade)
   @student_name = student_name
   @grade = grade
-  @roster[@grade] ||= []
-  @roster[@grade] << @student_name
+  roster[grade] ||= []
+  roster[grade] << student_name
  end 
  
  def grade(grade)
-   @roster[grade]
+   roster[grade]
  end 
  
  def sort
